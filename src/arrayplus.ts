597e01d25a74a8c32602e0c269cbfc9923fb1789
Dalci de Jesus Bagolin <dalcib@users.noreply.github.com>
2016-05-26 17:29:40 -0400

    Object.assign polyfill

--- src/arrayplus.ts
+++ src/arrayplus.ts
@@ -300,3 +300,32 @@ if (!Array.prototype.fill) {
     return Object.prototype.toString.call(arg) === '[object Array]';
   };
 }*/
+
+
+interface ObjectConstructor {
+    assign(target: any, ...sources: any[]): any;
+}
+
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
