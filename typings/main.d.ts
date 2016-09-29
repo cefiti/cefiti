85576bd0c5e441b5251ea136796da98f17f826b4
Dalci de Jesus Bagolin <dalci.bagolin@redemapa.agro>
2016-09-29 18:34:06 -0300

    Change types to npm @types

--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/expect/index.d.ts" />
-/// <reference path="main/ambient/mocha/index.d.ts" />
-/// <reference path="main/ambient/react-dom/index.d.ts" />
-/// <reference path="main/ambient/react/index.d.ts" />
