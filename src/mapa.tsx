ba643793ded683e027a4986f63e21a2eaf6191af
Dalci de Jesus Bagolin <dalci.bagolin@redemapa.agro>
2016-11-03 16:36:56 -0300

    Remove db.ts

--- src/mapa.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import uiStore from './uistore'
-import {observer} from 'mobx-react'
-
-var Mapa = ({}) => (uiStore.exibeMapa ? (<div className="center"><img src="moko_sigatoka_n.png" width="60%"/></div>) : null)
-
-
-export default observer(Mapa);
