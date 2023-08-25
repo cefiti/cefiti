c101ea3aba141eddc8b25120ce3c04b00952aec5
Dalci de Jesus Bagolin <dalcib@users.noreply.github.com>
2023-08-25 13:31:13 -0300

    Simplify useState

--- src/useState.ts
+++ src/useState.ts
@@ -1,25 +1,19 @@
 import { proxy, useSnapshot } from 'valtio'
 
 export function useState<T extends object>(store: T) {
-    const snapshot = useSnapshot(store)
-    return new Proxy(store, {
-      set(target, prop, value, receiver) {
-        Reflect.set(target, prop, value, receiver)
-        return true
-      }, 
-      get: function (target, prop, receiver) {
-        const value = Reflect.get(snapshot, prop, receiver)
-         if (typeof value === 'function') {
-          return function (...args: any) {
-            //Reflect.apply(target, target, argumentsList) 
-            //@ts-ignore
-            return target[prop].apply(store, args) 
-          }
-        } else {
-          return value
-        } 
-      },
-    })
-  }
-
-  export {proxy} 
+  const snapshot = useSnapshot(store)
+  return new Proxy(store, {
+    set(target, prop, value) {
+      Reflect.set(target, prop, value)
+      return true
+    }, 
+    get(target, prop) {
+      const value = Reflect.get(snapshot, prop)
+       if (typeof value === 'function') {
+        return  (...args: any) => value.apply(target, args) 
+      } else {
+        return value
+      } 
+    },
+  })
+}
